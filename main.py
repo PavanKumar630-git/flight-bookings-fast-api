161e948c07fd731ad4534f8b7c270ab3d7f26baa
PavanKumar630-git <pavankumar.scarping@gmail.com>
2025-04-05 15:36:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -118,3 +118,65 @@ def get_spicejet_booking_details(pnr: str = Query(...), lastname: str = Query(..
         return response.json()
     except Exception as e:
         return {"error": str(e)}
+
+
+
+@app.get("/digital_kuwaitairways")
+def get_digital_kuwaitairwaysdetails(pnr: str = Query(...), lastname: str = Query(...)):
+    headers = {
+        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+        'Referer': f'https://www.spicejet.com/trips/details?pnr={pnr}&last={lastname}',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        'Content-Type': 'application/json',
+        'os': 'desktop',
+    }
+    params = {'recordLocator': pnr, 'lastName': lastname}
+    json_data = {'userData': {}, 'method': 'GET'}
+
+    try:
+        response = requests.post('https://www.spicejet.com/api/v1/booking/retrieveBookingByPNR', params=params, headers=headers, json=json_data)
+        return response.json()
+    except Exception as e:
+        return {"error": str(e)}
+
+
+@app.get("/shop_lufthansa")
+def get_shop_lufthansa_details(pnr: str = Query(...), lastname: str = Query(...)):
+    airindia_token = get_airindia_oauth_token()
+
+    headers = {
+        'accept': 'application/json',
+        'authorization': f"{airindia_token['token_type']} {airindia_token['access_token']}",
+        'content-type': 'application/json',
+        'origin': 'https://travel.airindia.com',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+    }
+    params = {'lastName': lastname, 'showOrderEligibilities': 'true', 'checkServicesAndSeatsIssuanceCurrency': 'false'}
+
+    try:
+        response = requests.get(f'https://api-des.airindia.com/v2/purchase/orders/{pnr}', params=params, headers=headers)
+        return response.json()
+    except Exception as e:
+        return {"error": str(e)}
+
+
+
+@app.get("/malaysiaairlines")
+def get_malaysiaairlines_booking_details(pnr: str = Query(...), lastname: str = Query(...)):
+    headers = {
+        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+        'Referer': f'https://www.spicejet.com/trips/details?pnr={pnr}&last={lastname}',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        'Content-Type': 'application/json',
+        'os': 'desktop',
+    }
+    params = {'recordLocator': pnr, 'lastName': lastname}
+    json_data = {'userData': {}, 'method': 'GET'}
+
+    try:
+        response = requests.post('https://www.spicejet.com/api/v1/booking/retrieveBookingByPNR', params=params, headers=headers, json=json_data)
+        return response.json()
+    except Exception as e:
+        return {"error": str(e)}
+
+
